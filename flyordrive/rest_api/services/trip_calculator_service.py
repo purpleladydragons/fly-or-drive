55560b54e4ce4bc233328ac574434e8e9a254f19
Austin Steady <asteady23@gmail.com>
2021-08-29 19:25:18 -0700

    partial work for incorporating drive to/from airport for fly trip

--- flyordrive/rest_api/services/trip_calculator_service.py
+++ flyordrive/rest_api/services/trip_calculator_service.py
@@ -24,6 +24,7 @@ class TripCalculatorService:
         self.eia = EIAGateway()
         self.sky = SkyScannerGateway()
 
+    # TODO handle latlng input as well
     def haversine(self, origin, destination):
         """
         Calculate the great circle distance in kilometers between two points
@@ -74,6 +75,39 @@ class TripCalculatorService:
 
         return FlyingInfo(flight_duration_minutes, avg_price)
 
+    def __calculate_flying_trip(self, origin, destination):
+        """
+        For cases when the origin or destination are a non-trivial distance from nearest airport
+
+        Geocode the places and determine nearest airports. Calculate flight between them and then
+        calculate driving routes to origin airport and from destination airport
+
+        :param origin:
+        :param destination:
+        :return:
+        """
+
+        origin_latlng = self.gdm.get_lat_lng(origin)
+        origin_airport = self.__find_nearest_airport(origin_latlng)
+
+        destination_latlng = self.gdm.get_lat_lng(destination)
+        destination_airport = self.__find_nearest_airport(destination_latlng)
+
+        # TODO make sure types are all right
+        drive_to_airport = self.__calculate_drive(origin, origin_airport)
+        flight_info = self.__calculate_flight(origin_airport, destination_airport)
+        drive_from_airport = self.__calculate_drive(destination_airport, destination)
+
+        # TODO combine the info into some coherent package
+
+    def __find_nearest_airport(self, coords):
+        # TODO look up airports from db
+        airports = []
+
+        dists = [(airport, self.haversine(coords, airport)) for airport in airports]
+        nearest_airport = sorted(dists, key=lambda d: d[1])[:3]
+        return nearest_airport
+
     # TODO add a decorate to specify how it should be serialized to json compatible view
     def calculate_trip(self, origin, destination, max_one_day_driving_minutes=8 * 60, car_mpg=20):
         """
@@ -102,6 +136,7 @@ class TripCalculatorService:
                 'gas_price_sum': driving_info.gas_total_price,
                 'total_price': driving_info.hotel_total_price + driving_info.gas_total_price,
             },
+            # TODO just nest a "driving" section under it
             'flying': {
                 'duration_minutes': flying_info.flight_duration_minutes,
                 'flight_price': flying_info.estimated_price,
